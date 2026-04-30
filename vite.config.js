7f4e468ab259aa6a9078d08f0ed679551d83001e
Tomo <Tomo.bernet@stud.hslu.ch>
2026-05-01 00:20:22 +0200

    fix base path for GitHub Pages namen geändert

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coinflip-game/'
+  base: '/Discussions-Community-Health-Environments-Deployments/'
 })
